54706076ef4481e0e2bbe1155d59cca566879225
lynzz <lynzz168@gmail.com>
2015-09-21 15:35:02 +0800

    spm-v3

--- sifter/Gruntfile.js
+++ sifter/Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
     copy: {
       main: {
         src: 'vendor/sifter/sifter.js',
-        dest: 'src/sifter.js',
+        dest: 'sifter.js',
         options: {
           process: function (content, srcpath) {
             content = 'define(function(require, exports, module) {\n' + content + '\n});';
